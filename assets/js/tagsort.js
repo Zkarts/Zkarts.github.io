68c45eda68dded6ae475bfc1825f45501af5fa16
Zkarts <enmolenaar@gmail.com>
2019-12-20 14:18:56 +0100

    Filtering with the tag buttons at the top

--- assets/js/tagsort.js
+++ assets/js/tagsort.js
@@ -1,7 +1,20 @@
-function HideAllPosts() {
+function OnClickTagButton(el) {
+	ShowAllPosts();
+	HidePostsWithoutTag(el.textContent);
+}
+
+function ShowAllPosts() {
+	var posts = document.getElementsByClassName("post-hidden");
+	for	(var i = 0; i < posts.length; i++) {
+		posts[i].classList.remove("post-hidden");
+	}
+}
+
+function HidePostsWithoutTag(tag) {
 	var posts = document.getElementsByClassName("post");
 	for	(var i = 0; i < posts.length; i++) {
-		if (i % 2 == 0) {
+		var taggedNode = posts[i].firstChild;
+		if(taggedNode.getAttribute("data-tags").split(', ').indexOf(tag) == -1) {
 			posts[i].classList.add("post-hidden");
 		}
 	}
